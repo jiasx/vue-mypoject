5697c3569ec6ef3ed3c60fc8fd76ea37d2e190ff
jiasx <jiasx@asiainfo.com>
2018-08-02 20:46:06 +0800

    master

--- src/main.js
+++ src/main.js
@@ -11,18 +11,18 @@ import moment from "moment"; //时间过滤  ???
 import VueMoment from "vue-moment"; //时间过滤
 import axios from 'axios' //数据处理  原来用vue-resource
 // import ElementUex from 'element-uex'
-import filter from './components/js/filters'
+
 // import 'element-uex/lib/theme-default/index.css'
 Vue.config.productionTip = false
 Vue.use(ElementUI);
-Vue.use(ElementUex);
+
 Vue.use(VueMoment, moment);
 Vue.prototype.$http = axios;
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  filter,
+  
   store:store,
     components: { App },
     template: '<App/>'
